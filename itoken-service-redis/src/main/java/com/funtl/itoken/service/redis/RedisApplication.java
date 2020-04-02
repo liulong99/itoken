fb721928d4b987582b819c1b2d05cf8c43e7501a
liulong <706750966@qq.com>
2020-04-03 00:08:14 +0800

    init commit

---
+++ itoken-service-redis/src/main/java/com/funtl/itoken/service/redis/RedisApplication.java
@@ -0,0 +1,19 @@
+package com.funtl.itoken.service.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Description :
+ * @Author 刘龙
+ * @Date 2020/4/2 23:46
+ * @Version 1.0
+ **/
+@SpringBootApplication
+@EnableEurekaClient
+public class RedisApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RedisApplication.class,args);
+    }
+}
--- itoken-web-admin/src/main/java/com/funtl/itoken/web/admin/service/fallback/AdminServiceFallback.java
+++ itoken-web-admin/src/main/java/com/funtl/itoken/web/admin/service/fallback/AdminServiceFallback.java
@@ -1,5 +1,6 @@
 package com.funtl.itoken.web.admin.service.fallback;
 
+import com.funtl.itoken.common.constants.HttpStatusConstants;
 import com.funtl.itoken.common.dto.BaseResult;
 import com.funtl.itoken.common.utils.MapperUtils;
 import com.funtl.itoken.web.admin.service.AdminService;
@@ -16,7 +17,10 @@ import org.springframework.stereotype.Component;
 public class AdminServiceFallback implements AdminService{
     @Override
     public String login(String loginCode, String password) {
-        BaseResult baseResult = BaseResult.notOk(Lists.newArrayList(new BaseResult.Error("502", "从上游服务器接收到无效的响应")));
+        BaseResult baseResult = BaseResult.notOk(Lists.newArrayList(
+                new BaseResult.Error(
+                        String.valueOf(HttpStatusConstants.BAD_GATEWAY.getStatus()),
+                        HttpStatusConstants.BAD_GATEWAY.getContent())));
         try {
             return MapperUtils.obj2json(baseResult);
         } catch (Exception e) {
