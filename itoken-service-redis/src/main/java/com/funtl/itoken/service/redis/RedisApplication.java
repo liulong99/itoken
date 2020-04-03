e9d5db35b4999017ce55e4fef629c1608816b4b3
liulong <706750966@qq.com>
2020-04-03 22:39:42 +0800

    init commit

--- itoken-service-redis/src/main/java/com/funtl/itoken/service/redis/RedisApplication.java
+++ itoken-service-redis/src/main/java/com/funtl/itoken/service/redis/RedisApplication.java
@@ -2,6 +2,7 @@ package com.funtl.itoken.service.redis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
@@ -10,7 +11,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  * @Date 2020/4/2 23:46
  * @Version 1.0
  **/
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableEurekaClient
 public class RedisApplication {
     public static void main(String[] args) {
